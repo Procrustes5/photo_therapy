6119211f97a9cf12ee8f4efa9fd975f1a9928651
Hanse Kim <procrustes5@gmail.com>
2024-03-22 01:46:50 +0900

    Update router.js

--- app/javascript/config/router.js
+++ app/javascript/config/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import HomePage from '@pages/UiHomePage.vue'
 
@@ -11,6 +11,6 @@ export const routes = [
 ];
 
 export const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
