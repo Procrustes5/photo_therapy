1bb49cc673e30af017e0245e4b87172152621690
Hanse Kim <procrustes5@gmail.com>
2024-03-24 00:09:05 +0900

    aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -95,4 +95,6 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   # config.hosts << 'www.photo-therapy.onrender.com'
+  Rails.application.config.hosts << 'phototherapy.kr'
+  Rails.application.routes.default_url_options[:port] = 3000
 end
