82c86de40ef6b9857da74ccf2dbbe4077c667a57
Hanse Kim <procrustes5@gmail.com>
2024-03-24 00:53:23 +0900

    Delete aws.rb

--- config/initializers/aws.rb
+++
@@ -1,9 +0,0 @@
-unless Rails.env.test? || Rails.env.development?
-  credentials = Aws::Credentials.new(
-    Rails.application.credentials.dig(:aws, :access_key_id),     # アクセスキーID
-    Rails.application.credentials.dig(:aws, :secret_access_key)  # シークレットアクセスキー
-  )
-
-  s3_resource = Aws::S3::Resource.new(region: Rails.application.credentials.dig(:aws, :s3, :region), credentials: Rails.application.credentials.dig(:aws, :s3, :bucket))
-  S3_BUCKET = s3_resource.bucket('S3のバケット名')
-end
