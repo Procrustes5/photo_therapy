c95a2483b1c91a0ff0f52012fb4f39131a02b8a4
Hanse Kim <procrustes5@gmail.com>
2024-03-31 14:35:23 +0900

    category frame

--- app/javascript/views/pages/UiGallery.vue
+++ app/javascript/views/pages/UiGallery.vue
@@ -1,17 +1,23 @@
 <script lang="ts" setup>
-
+const images = [1 ,1 ,1 ,1, 1, 1, 1, 1]
 </script>
 <template>
   <div class="main-wrapper">
-    <div class="main-image">
-      
+    <div class="main-image-wrapper">
+      <div class="main-image">
+        
+      </div>
     </div>
     <div class="content-wrapper">
       <div class="content-header">
-        
+        <span>This page is preparing to release</span>
       </div>
       <div class="content">
-        <div>
+        <div 
+          v-for="(image, index) in images"
+          :key="index"
+          class="content-img"
+        >
           
         </div>
       </div>
@@ -19,5 +25,47 @@
   </div>
 </template>
 <style lang="scss" scoped>
-  
+  .main-wrapper {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .main-image-wrapper {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      .main-image {
+        width: 80%;
+        height: 50vh;
+        background: red;
+      }
+    }
+    .content-wrapper {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .content-header {
+        margin: 15px 0px;
+        width: 80%;
+        span {
+          color: whitesmoke;
+          font-size: 18px;
+        }
+      }
+      .content {
+        width: 80%;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        gap: 5px;
+        padding: 0px 30px;
+        .content-img {
+          background: blue;
+          height: 150px;
+        }
+      }
+    }
+  }
 </style>
