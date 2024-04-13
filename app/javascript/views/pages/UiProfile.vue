8753d69896e9beacad1e29f5c6f1080ffb2500d5
Hanse Kim <procrustes5@gmail.com>
2024-04-13 10:35:14 +0900

    Update UiProfile.vue

--- app/javascript/views/pages/UiProfile.vue
+++ app/javascript/views/pages/UiProfile.vue
@@ -3,11 +3,22 @@ import Menu from '@view/ui-elements/UiMenuDrawer.vue'
 
 </script>
 <template>
-  <div>
-    
+  <div class="profile-wrapper">
+    <h1>Photographer</h1>
+    <h3>Kim Han</h3>
   </div>
   <Menu></Menu>
 </template>
 <style lang="scss" scoped>
-  
+  h1, h3 {
+    color: whitesmoke;
+  }
+  .profile-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    width: 100%;
+    padding: 0px 15px;
+    box-sizing: border-box;
+  }
 </style>
