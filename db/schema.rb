0ab4cbac3e267a37d4a34479216815d12f1343cd
Hanse Kim <procrustes5@gmail.com>
2024-04-27 10:34:36 +0900

    Revert "devise 도입"
    This reverts commit 44ade87e94f37cdc7d85e0d0580b9b0c7b4f69af.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_21_131252) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_23_120953) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,13 +48,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_21_131252) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "homes", force: :cascade do |t|
-    t.string "title"
-    t.string "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "photos", force: :cascade do |t|
     t.bigint "category_id", null: false
     t.string "name", null: false
@@ -66,16 +59,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_21_131252) do
   create_table "users", force: :cascade do |t|
     t.string "last_name", limit: 50, null: false
     t.string "first_name", limit: 50, null: false
+    t.string "email", null: false
     t.boolean "is_admin", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
